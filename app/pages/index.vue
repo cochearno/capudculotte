889fc9d7f5b2287b0b9445a41ea1eb1da92d4eaa
cochearno <cochearno17@gmail.com>
2025-10-31 13:05:52 +0100

    add cta-menu params

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,8 @@
-<template></template>
+<template>
+	<div class="title-page">
+		<div class="title"><h1>Bienvenue, sur le site de capud'culotte</h1></div>
+	</div>
+</template>
 
 <script></script>
 
