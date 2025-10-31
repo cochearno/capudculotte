9738013b80ceef4b2ce88d6e08c1914b961cd936
cochearno <cochearno17@gmail.com>
2025-10-31 13:06:21 +0100

    add cta-menu params

--- app/components/elements/header/header.vue
+++ app/components/elements/header/header.vue
@@ -10,13 +10,39 @@
 					/>
 				</div>
 				<div class="title">CAPUD'CULOTTE</div>
+				<div class="page-heading-menu">
+					<elements-menu-cta
+						v-for="button in buttonList"
+						v-bind="button"
+						@cta-redirect="handleClick"
+					/>
+				</div>
 			</div>
 		</div>
 	</header>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		const buttonList = [
+			{ text: "Accueil", interact: "redirect", redirectUrl: "/" },
+			{ text: "Collections", interact: "redirect", redirectUrl: "/" },
+			{ text: "Tissus", interact: "redirect", redirectUrl: "/" },
+			{ text: "Contacts", interact: "redirect", redirecturl: "/" },
+		];
+		return {
+			buttonList,
+		};
+	},
+	methods: {
+		handleClick(item) {
+			if (item.interact === "redirect") {
+				navigateTo(item.redirectUrl);
+			}
+		},
+	},
+};
 </script>
 
 <style lang="scss" src="./header.scss" />
---
+++ app/components/elements/menu-cta/menu-cta.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="button-menu-heading" @click="handleRedirect">{{ text }}</div>
+</template>
+
+<script lang="js">
+export default {
+  name: "button-menu",
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+		interact: {
+			type: String,
+			default: "redirect"
+		},
+		redirectUrl: {
+			type: String,
+			default: undefined
+		}
+  },
+	emits: ["cta-redirect"],
+	methods: {
+		handleRedirect() {
+			const redirectData = {
+				interact: this.interact,
+				redirectUrl : this.redirectUrl
+			}
+			this.$emit("cta-redirect", redirectData)
+		}
+	}
+};
+</script>
+
+<style lang="scss" src="../menu-cta/menu-cta.scss" />
+
